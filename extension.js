e9b4e7bd0d037083bba4f207d6a4f80cd62b93f8
Eric Zeiberg <ejzeiberg@gmail.com>
2016-04-11 19:56:57 -0400

    Finish total points grade parser

--- extension.js
+++ extension.js
@@ -1,9 +1,12 @@
 var totalPoints
 var categories = []
+var currentGrade
+var numerator = 0
+var denominator = 0
 
 function parseWeightingSystem() {
     weightNodes = document.querySelectorAll('[data-prtname="Q1"]')
-    if (weightNodes.length > 0){
+    if (weightNodes.length > 1){
       totalPoints = false
       for (i = 0; i < weightNodes.length; i++){
         node = weightNodes[i]
@@ -22,23 +25,26 @@ function parseWeightingSystem() {
 
 function calculateCurrentGrade(){
   if (totalPoints){
-    assignmentNodes = document.getElementById("assignmentScores").childNodes
-    for (i = 1; i < assignmentNodes.length; i++){
-      node = assignmentNodes[i]
-      if (node.tagName === "tr"){
+    assignmentNodes = document.getElementById("assignmentScores").getElementsByTagName("table")[0].getElementsByTagName("tbody")[0].childNodes
+    for (i = 2; i < assignmentNodes.length; i+=2){
+        node = assignmentNodes[i]
         colValues = node.childNodes
-        score = colValue[4]
-        
-      }
+        score = colValues[9].textContent
+        console.log(score)
+        numerator += parseFloat(score.split("/")[0])
+        denominator += parseFloat(score.split("/")[1])
     }
+    currentGrade = (numerator/denominator)
+    console.log(currentGrade)
   }
 }
 
-function insertHTML(){
-   htmlToInsert ='<table id="points" border=1><tr><th style="z-index:1; text-align:center">Earned Points</th><th align="center">Total Points</th><th align="center">Update Grade</th></tr>';
-   htmlToInsert +='<tr><td align="center"><input style="text-align:center" value="1" type="text" id=\"Earn\"></td>';
-   htmlToInsert +='<td align="center"><input style="text-align:center" value="2" type="text" id=\"Tot\"></td>';
-   htmlToInsert +='<td align="center"><input style="text-align:center" value="3" type="text" id=\"Percent\"></td>';
+function insertTotalPointsHTML(){
+  htmlToInsert = '<form style="padding-left:22px" action=""><em>Total points grading system detected</em><br></form><br>';
+   htmlToInsert +='<table id="points" border=1><tr><th style="z-index:1; text-align:center">Earned Points</th><th align="center">Total Points</th><th align="center">Update Grade</th></tr>';
+   htmlToInsert +='<tr><td align="center"><input style="text-align:center" value="'+numerator+'" type="text" id=\"Earn\"></td>';
+   htmlToInsert +='<td align="center"><input style="text-align:center" value="'+denominator+'" type="text" id=\"Tot\"></td>';
+   htmlToInsert +='<td align="center"><input type="button" id="calcButton" value="Update Final Grade"></td>';
 
    var content = document.getElementById("content-main");
    var newNode = document.createElement("div");
@@ -54,5 +60,6 @@ function insertHTML(){
 
 $(document).ready(function() {
   parseWeightingSystem()
-  insertHTML()
+  calculateCurrentGrade()
+  insertTotalPointsHTML()
 })
