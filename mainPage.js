2e2186019b989079b44caaf1bca2e5c36a247917
Eric Zeiberg <ejzeiberg@gmail.com>
2016-05-13 13:24:37 -0400

    More progress on GPA calc

--- mainPage.js
+++ mainPage.js
@@ -1,7 +1,7 @@
 var amountOfColumms = 0
 var amountOfCourses = 0
 var courseData = []
-
+var courseNames = []
 
 function parsePage(){
   outerNode = document.getElementsByClassName("center th2")[0]
@@ -16,6 +16,14 @@ function parsePage(){
   }
 
   gridNode = document.getElementsByClassName("grid")[0]
+  tdElements = gridNode.getElementsByTagName("td");
+  for (i = 0; i < tdElements.length; i++){
+      if (tdElements[i].hasAttribute("align")) {
+        courseNames.push(tdElements[i].textContent)
+    }
+  }
+  
+  
   links = gridNode.getElementsByTagName("a");
   tempData = []
   for (i = 0; i < links.length; i++){
@@ -24,7 +32,8 @@ function parsePage(){
           tempData.push("--")
         }
         else {
-          score = links[i].textContent.replace(/[^\d.-]/g, '');
+         // score = links[i].textContent.replace(/[^\d.-]/g, '');
+             score = links[i].textContent
           tempData.push(score)
         }
       }
@@ -34,6 +43,72 @@ function parsePage(){
     courseData.push(temparray)
   }
   console.log(courseData)
+  
+  for (i = 0; i < courseData[0].length; i++){
+      currentGPATotal = 0
+      courseTotal = 0
+    for (j = 0; j < (amountOfColumms); j++){
+            score = courseData[j][i]
+            if (score === "--"){
+                continue;
+            }
+            else {
+                if (score.search("A") != -1){
+                    currentGPATotal += 4.0;
+                    courseTotal+=1;
+                    if (courseNames[i].search("AP") != -1) {
+                        currentGPATotal += 0.5;
+                    }
+                    else if (courseNames[i].search("Honors") != -1) {
+                        currentGPATotal += 0.25;
+                    }
+                }
+                else if (score.search("B") != -1){
+                    currentGPATotal += 3.0;
+                    courseTotal+=1;
+                     if (courseNames[i].search("AP") != -1) {
+                        currentGPATotal += 0.5;
+                    }
+                    else if (courseNames[i].search("Honors") != -1) {
+                        currentGPATotal += 0.25;
+                    }
+                }
+                else if (score.search("C") != -1){
+                    currentGPATotal += 2.0;
+                    courseTotal+=1;
+                     if (courseNames[i].search("AP") != -1) {
+                        currentGPATotal += 0.5;
+                    }
+                    else if (courseNames[i].search("Honors") != -1) {
+                        currentGPATotal += 0.25;
+                    }
+                }
+                else if (score.search("D") != -1){
+                    currentGPATotal += 2.0;
+                    courseTotal+=1;
+                     if (courseNames[i].search("AP") != -1) {
+                        currentGPATotal += 0.5;
+                    }
+                    else if (courseNames[i].search("Honors") != -1) {
+                        currentGPATotal += 0.25;
+                    }
+                }
+                else if (score.search("F") != -1){
+                    currentGPATotal += 1.0;
+                    courseTotal+=1;
+                     if (courseNames[i].search("AP") != -1) {
+                        currentGPATotal += 0.5;
+                    }
+                    else if (courseNames[i].search("Honors") != -1) {
+                        currentGPATotal += 0.25;
+                    }
+                }
+    
+            }
+        }
+        console.log(currentGPATotal / courseTotal)
+    }
+    
 }
 
 
